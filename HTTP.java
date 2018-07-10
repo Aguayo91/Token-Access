7b4d86deeecc8a54fbd8ebc9efd34ad208c45b64
Christian Aguayo <caguayo@elektra.com.mx>
2018-07-10 10:42:36 -0500

    Libro Git & GitHub

--- HTTP.java
+++ HTTP.java
@@ -1,8 +1,16 @@
 
-HttpsURLConnection
+    HttpsURLConnection
 'http://www.androidcurso.com/index.php/recursos/43-unidad-10-internet-sockets-http-y-servicios-web/325-utilizando-http-desde-android'
 
 
 
-HttpURLConnection
+    HttpURLConnection
 'http://www.hermosaprogramacion.com/2015/01/android-httpurlconnection/'
+
+
+    Control de versiones con Git y GitHub
+'https://manuais.iessanclemente.net/index.php/Control_de_versiones_con_Git_y_GitHub'
+
+
+    Libro Git --distributed-even-if-your-workflow-isnt
+'https://git-scm.com/book/en/v2'
